5e6f9bf263fed9c037c34be24ac96ffd0a2877a6
Daniel Fields <dgfields4193@gmail.com>
2023-03-10 10:03:09 -0500

    wip: removes commented-out route

--- controllers/Stocks.js
+++ controllers/Stocks.js
@@ -11,30 +11,14 @@ const getStocks = async () => {
   const response = await fetch(url);
   const data = await response.json();
   stockData = data
-  // stockData.push(data);
 }
 getStocks();
-
-
-// router.get('/stocks/seed', async (req, res) => {
-//   getStocks();
-//   const allStocks = stockData[0];
-//   Promise.all(allStocks.map(async (stock) => {
-//     req.body = stock
-//       try {
-//         await
-//         res.status(200).json(await Stock.create(req.body));
-//       } catch (error) {
-//         res.status(400).json({ message: "something went wrong" });
-//       }
-//   }));
-// })
 
 router.get('/stocks/seed', (req, res) => {
   getStocks()
   Stock.create(stockData)
   res.send('seeded')
-})
+});
 
 router.get("/", (req, res) => {
   res.send('hello investing world');
