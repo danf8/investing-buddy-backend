d4f4ad3ac64b123e18b9a99a4e51e5dbf26c84fc
Kathy <kath.scholle@gmail.com>
2023-03-15 19:12:59 -0400

    adds 2 props in userschema

--- models/User.js
+++ models/User.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    ownedStocks: [
+        {
+          type: Array
+        }
+      ],
+    currentMoney: {
+        type: Number,
+        required: true
+      }
+    }, {timestamps: true});
+
+module.exports = mongoose.model('User', userSchema);
