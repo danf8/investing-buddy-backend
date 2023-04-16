3e665343a67d0995ec1b1f53770c3af9af872cc1
Daniel Fields <dgfields4193@gmail.com>
2023-04-16 17:40:57 -0400

    wip: prepare to deploy on heroku

--- server.js
+++ server.js
@@ -23,15 +23,15 @@ PORT, DATABASE_URL, GOOGLE_PRIVATE_ID, GOOGLE_PRIVATE_KEY, GOOGLE_CLIENT_ID
 admin.initializeApp({
   credential: admin.credential.cert({
     "type": "service_account",
-    "project_id": "investing-buddy-app",
-    "private_key_id": GOOGLE_PRIVATE_ID,
+    "project_id": "investing-app-1",
+    "private_key_id": GOOGLE_PRIVATE_KEY,
     "private_key": GOOGLE_PRIVATE_KEY.replace(/\n/g, ''),
-    "client_email": "firebase-adminsdk-q0oiv@investing-buddy-app.iam.gserviceaccount.com",
+    "client_email": "firebase-adminsdk-u6tiu@investing-app-1.iam.gserviceaccount.com",
     "client_id": GOOGLE_CLIENT_ID,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-q0oiv%40investing-buddy-app.iam.gserviceaccount.com"
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-u6tiu%40investing-app-1.iam.gserviceaccount.com"
   })
 })
 
