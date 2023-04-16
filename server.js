44d77c789c5559971d48eb5ac37e0c1dc70e04ad
Daniel Fields <dgfields4193@gmail.com>
2023-04-16 18:14:46 -0400

    wip: handle deployment errors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ admin.initializeApp({
     "type": "service_account",
     "project_id": "investing-app-1",
     "private_key_id": GOOGLE_PRIVATE_KEY,
-    "private_key": GOOGLE_PRIVATE_KEY.replace(/\n/g, ''),
+    "private_key": GOOGLE_PRIVATE_KEY.replace(/\n/g, '\n'),
     "client_email": "firebase-adminsdk-u6tiu@investing-app-1.iam.gserviceaccount.com",
     "client_id": GOOGLE_CLIENT_ID,
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
