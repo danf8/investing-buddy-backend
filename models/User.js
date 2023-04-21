cf0f3e3ddc53f8829b5d838eb9ef0cadf76e276b
Daniel Fields <dgfields4193@gmail.com>
2023-04-21 14:05:01 -0400

    wip: create stock purchased model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
+const stockPurchased = new Schema({
+  name: {type: String},
+  price: {type: Number},
+  currentPrice: {type: Number},
+  symbol: {type: String},
+  ownedShares: {type: Number},
+});
+
 const userStocks = new Schema({
-    ownedStocks: [
-        {
-          type: Array,
-        }
-      ],
+    ownedStocks: [stockPurchased],
     currentMoney: {
         type: Number,
         required: true
